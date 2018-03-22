c98b0365f82b1e2c22be681153a14e151b045473
Desenvolvimento <Desenvolvimento@DESKTOP-24SVAOL>
2018-03-22 08:15:07 -0300

    Web com XML

---
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,44 @@
+package br.com.alura.loja.modelo;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Projeto {
+
+	private long id;
+	private String nome;
+	private int ano;
+
+	public Projeto(long l, String string, int i) {
+		this.id = l;
+		this.nome = string;
+		this.ano = i;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public String toXML() {
+		return new XStream().toXML(this);
+	}
+}
